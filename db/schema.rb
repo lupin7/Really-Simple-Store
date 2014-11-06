6a36fa49a48e995e70cf7d91478eee8d0996c4fd
Marc Estrada <mestrada36@academic.rrc.ca>
2014-11-06 14:40:39 -0800

    Added image_filenames to the Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106211542) do
+ActiveRecord::Schema.define(version: 20141106221828) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141106211542) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_filename"
   end
 
 end

ae3cc13893e7a9fa4a66fba55e63e37844481403
Marc Estrada <mestrada36@academic.rrc.ca>
2014-11-06 13:57:46 -0800

    We now have a partial for products display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+	validates :name, :description, :price, presence: true
+	validates :name, uniqueness: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
